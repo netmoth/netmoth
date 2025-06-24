42fe251bb21450c3adf33e248b6f8f46f21e4bed
Dmitry Shurco <shurco@me.com>
2025-06-24 14:13:00 +0000

    🔧 chore: Remove unused random seed initialization
    - Deleted the random seed initialization in `main.go` as it was not utilized in the current implementation.

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,16 +3,12 @@ package main
 import (
 	"flag"
 	"log"
-	"math/rand"
-	"time"
 
 	"github.com/netmoth/netmoth/internal/config"
 	"github.com/netmoth/netmoth/internal/sensor"
 )
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-
 	var configPath string
 	flag.StringVar(&configPath, "cfg", "config.yml", "configuration file path")
 	flag.Parse()
